da7e82aef550406f00dce6f4fddab10ad6d2d661
zedj22 <109031571+zedj22@users.noreply.github.com>
2024-01-19 18:09:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 app.use("/api", require('./routes/app.routes'));
+app.get("/", (req, res) => {
+  res.send("Welcome to the root path!");
+});
 
 const port = 3000;
 
